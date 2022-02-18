dcbde604227cbd63fb57565d91b5d2eaa747f31e
tcloudlabs <99907810+tcloudlabs@users.noreply.github.com>
2022-02-18 21:34:45 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
-
-echo"hai";
-
+echo"<h1 align='center'> MY FIRST AZURE WEBAPP USING SQLSERVER & PHP </h1>";
 ?>
+
+<html>
+<body bgcolor="yellow">
+<hr>
+<br>
+<h2 align="Center">EMPLOYEE FORM</h2>
+
+<form align="center" name="employee" action="insert.php">
+  <label for="fname">Name:</label><br>
+  <input type="text" id="name" name="name" ><br>
+  <label for="lname">AGE:</label><br>
+  <input type="text" id="age" name="age"><br><br>
+  <label for="lname">EMAIL:</label><br>
+  <input type="text" id="email" name="email"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
+
+
+
